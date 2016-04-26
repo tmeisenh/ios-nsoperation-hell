4f68510d528cc74f4fb0922b725e596de951a396
Travis Meisenheimer <travis@indexoutofbounds.com>
2016-04-25 21:56:52 -0500

    Capture method call stack and NSLog it in dealloc to see wtf is going on.

--- NSOperationsHell/NSOperationsHell/SleepingAsyncOperation.m
+++ NSOperationsHell/NSOperationsHell/SleepingAsyncOperation.m
@@ -1,10 +1,29 @@
 #import "SleepingAsyncOperation.h"
 
+@interface SleepingAsyncOperation()
+
+@property (nonnull) NSMutableArray *call;
+
+@end
+
 @implementation SleepingAsyncOperation
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _call = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    NSLog(@"stack %@ %p", _call, self);
+}
+
 - (void)start {
+    [_call addObject:@"start"];
     if (self.isCancelled) {
-        NSLog(@"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Cancelled before starting");
+        NSLog(@"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Cancelled before starting %p", self);
         [self transitionToFinishedState];
         return;
     }
@@ -14,24 +33,29 @@
 }
 
 - (void)main {
-    NSLog(@"---------------------------------------------- Main");
+    [_call addObject:@"main"];
+    NSLog(@"---------------------------------------------- Main %p", self);
     [self dowork];
 }
 
 - (void)dowork {
+    [_call addObject:@"dowork"];
     [NSThread sleepForTimeInterval:1];
     [self transitionToFinishedState];
 }
 
 - (void)cancel {
     [super cancel];
+    [_call addObject:@"cancel"];
     if (self.isReady && !self.isExecuting && !self.isFinished) {
-        NSLog(@"################################### Cancel w/o starting");
-        /* transitionToStartedState fixex: went isFinished=YES without being started by the queue it is in */
         [self transitionToStartedState];
         [self transitionToFinishedState];
+        NSLog(@"################################### Cancel w/o starting %p", self);
+        /* transitionToStartedState fixes: went isFinished=YES without being started by the queue it is in */
+//        [self transitionToStartedState];
+//        [self transitionToFinishedState];
     } else {
-        NSLog(@"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Cancel after starting");
+        NSLog(@"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Cancel after starting %p", self);
         // I'm finished, so I don't care.
         // I'm not ready which in this test case isn't possible
         // I'm executing in which case I'll just finish as normal.
